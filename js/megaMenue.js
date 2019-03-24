c84d9fcd0317b4e6b3775f43121c405f5460a3eb
Andrey <andrey.o@list.ru>
2019-03-24 15:54:43 +0500

    first commit

--- js/megaMenue.js
+++
@@ -1,9 +0,0 @@
-$('.hideMenue').hover(
-    function () {
-        $('.megaMenue').slideDown(500);
-        $('.megaMenue').css({"display":"flex"});
-    },
-    function () {
-        $('.megaMenue').slideUp(500);
-
-    });
--- js/singlepage.js
+++
@@ -1,7 +0,0 @@
-function include(url) {
-        var script = document.createElement('script');
-        script.src = url;
-        document.getElementsByTagName('head')[0].appendChild(script);
-    }
-include('js/browse.js');
-include('js/megaMenue.js');
